a64289d0a6d672787a286432fb950cf1a05b2e71
howtungtung <howtungtung@gmail.com>
2020-07-28 13:42:38 +0800

    1. initial version

---
+++ Assets/SafeAreaHelper/SafeAreaHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+namespace HowTungTung
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeAreaHelper : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        private Rect lastSafeArea;
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void LateUpdate()
+        {
+            if (lastSafeArea != Screen.safeArea)
+            {
+                lastSafeArea = Screen.safeArea;
+                Refresh();
+            }
+        }
+
+        public void Refresh()
+        {
+            var anchorMin = lastSafeArea.position;
+            var anchorMax = lastSafeArea.position + lastSafeArea.size;
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+            rectTransform.anchorMin = anchorMin;
+            rectTransform.anchorMax = anchorMax;
+        }
+    }
+}
+
